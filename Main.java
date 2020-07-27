91b7bd212f436f611027833fa6b40f549cb9538c
mounikalluru <60348882+mounikalluru@users.noreply.github.com>
2020-07-27 13:36:01 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DeleteCharacter deleteAChar = new DeleteCharacter();
+		System.out.println(deleteAChar.deleteA("This is A String"));
+
+	}
+
+}
